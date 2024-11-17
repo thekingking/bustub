ce52de3b128791031992aa1ff3d57697b44cbb33
thekingking <yinjinsong365@gmail.com>
2024-11-17 12:04:46 +0800

    make format

--- src/execution/sort_executor.cpp
+++ src/execution/sort_executor.cpp
@@ -4,42 +4,40 @@ namespace bustub {
 
 SortExecutor::SortExecutor(ExecutorContext *exec_ctx, const SortPlanNode *plan,
                            std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx),
-    plan_(plan),
-    child_executor_(std::move(child_executor)) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
 
-void SortExecutor::Init() { 
-    child_executor_->Init();
-    tuples_.clear();
-    Tuple tuple;
-    RID rid;
-    while (child_executor_->Next(&tuple, &rid)) {
-        tuples_.emplace_back(tuple);
+void SortExecutor::Init() {
+  child_executor_->Init();
+  tuples_.clear();
+  Tuple tuple;
+  RID rid;
+  while (child_executor_->Next(&tuple, &rid)) {
+    tuples_.emplace_back(tuple);
+  }
+  std::sort(tuples_.begin(), tuples_.end(), [this](const Tuple &lhs, const Tuple &rhs) {
+    for (const auto &order_by : plan_->GetOrderBy()) {
+      auto order_by_type = order_by.first;
+      auto expr = order_by.second.get();
+      auto lhs_val = expr->Evaluate(&lhs, plan_->OutputSchema());
+      auto rhs_val = expr->Evaluate(&rhs, plan_->OutputSchema());
+      if (lhs_val.CompareLessThan(rhs_val) == CmpBool::CmpTrue) {
+        return order_by_type != OrderByType::DESC;
+      }
+      if (lhs_val.CompareGreaterThan(rhs_val) == CmpBool::CmpTrue) {
+        return order_by_type == OrderByType::DESC;
+      }
     }
-    std::sort(tuples_.begin(), tuples_.end(), [this](const Tuple &lhs, const Tuple &rhs) {
-        for (const auto &order_by : plan_->GetOrderBy()) {
-            auto order_by_type = order_by.first;
-            auto expr = order_by.second.get();
-            auto lhs_val = expr->Evaluate(&lhs, plan_->OutputSchema());
-            auto rhs_val = expr->Evaluate(&rhs, plan_->OutputSchema());
-            if (lhs_val.CompareLessThan(rhs_val) == CmpBool::CmpTrue) {
-                return order_by_type != OrderByType::DESC;
-            }
-            if (lhs_val.CompareGreaterThan(rhs_val) == CmpBool::CmpTrue) {
-                return order_by_type == OrderByType::DESC;
-            }
-        }
-        return true;
-    });
-    curr_pos_ = 0;
+    return true;
+  });
+  curr_pos_ = 0;
 }
 
-auto SortExecutor::Next(Tuple *tuple, RID *rid) -> bool { 
-    if (curr_pos_ >= tuples_.size()) {
-        return false;
-    }
-    *tuple = tuples_[curr_pos_];
-    ++curr_pos_;
-    return true; 
+auto SortExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  if (curr_pos_ >= tuples_.size()) {
+    return false;
+  }
+  *tuple = tuples_[curr_pos_];
+  ++curr_pos_;
+  return true;
 }
 }  // namespace bustub
--- src/optimizer/sort_limit_as_topn.cpp
+++ src/optimizer/sort_limit_as_topn.cpp
@@ -1,7 +1,7 @@
-#include "optimizer/optimizer.h"
 #include "execution/plans/limit_plan.h"
 #include "execution/plans/sort_plan.h"
 #include "execution/plans/topn_plan.h"
+#include "optimizer/optimizer.h"
 
 namespace bustub {
 
@@ -16,12 +16,8 @@ auto Optimizer::OptimizeSortLimitAsTopN(const AbstractPlanNodeRef &plan) -> Abst
     auto child = limit_plan.GetChildAt(0);
     if (child->GetType() == PlanType::Sort) {
       const auto &sort_plan = dynamic_cast<const SortPlanNode &>(*child);
-      return std::make_shared<TopNPlanNode>(
-        sort_plan.output_schema_,
-        sort_plan.GetChildPlan(),
-        sort_plan.GetOrderBy(),
-        limit_plan.GetLimit()
-      );
+      return std::make_shared<TopNPlanNode>(sort_plan.output_schema_, sort_plan.GetChildPlan(), sort_plan.GetOrderBy(),
+                                            limit_plan.GetLimit());
     }
   }
 
