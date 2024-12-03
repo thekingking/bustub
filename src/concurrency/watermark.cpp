d4b8063f868c6d622db95744afd5ba9d11fc700d
thekingking <yinjinsong365@gmail.com>
2024-12-03 15:32:13 +0000

    p4 t1 finish

--- src/concurrency/watermark.cpp
+++ src/concurrency/watermark.cpp
@@ -8,12 +8,29 @@ auto Watermark::AddTxn(timestamp_t read_ts) -> void {
   if (read_ts < commit_ts_) {
     throw Exception("read ts < commit ts");
   }
-
   // TODO(fall2023): implement me!
+  if (current_reads_.empty()) {
+    watermark_ = read_ts;
+  }
+  ++current_reads_[read_ts];
 }
 
 auto Watermark::RemoveTxn(timestamp_t read_ts) -> void {
   // TODO(fall2023): implement me!
+  auto iter = current_reads_.find(read_ts);
+  if (iter == current_reads_.end()) {
+    throw Exception("read ts not found");
+  }
+  if (iter->second == 1) {
+    current_reads_.erase(iter);
+  } else {
+    --iter->second;
+  }
+  if (read_ts == watermark_ && !current_reads_.empty()) {
+    while (watermark_ < commit_ts_ && current_reads_.find(watermark_) == current_reads_.end()) {
+      ++watermark_;
+    }
+  }
 }
 
 }  // namespace bustub
